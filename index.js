925cb7f70d5e227af972ce2d40a15166bf5e441c
noufalcm425 <111962252+noufalcm425@users.noreply.github.com>
2022-12-03 15:06:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const hourEL= document.getElementById("hour");
+const minnteEL=document.getElementById("minutes");
+
+const secondEl=document.getElementById("seconds");
+const ampmEl=document.getElementById("ampm");
+
+
+function updateClock(){
+    let h =new Date().getHours()
+    let m =new Date().getMinutes()
+    let s= new Date().getSeconds()
+    let ampm ="AM"
+
+    if (h>12) {
+        h=h-12
+        ampm="PM"
+        
+    }
+    h=h < 10? "0" + h :h;
+    m=m < 10? "0" + m :m;
+    s=s < 10? "0" + s :s;
+
+    hourEL.innerText=h;
+    minnteEL.innerText=m
+    secondEl.innerText=s
+    ampmEl,(innerText=ampm);
+    setTimeout(()=>{
+        updateClock()
+
+    },1000)
+}
+
+updateClock()
+
+
